b59a1b384db5ef54f5a8d7bc001b01cfc8b37ec4
Luis Park <road2nowhere@naver.com>
2018-07-01 12:29:21 +0000

    rolling update

--- node/server.js
+++ node/server.js
@@ -1,15 +1,17 @@
 'use strict';
-const express = require('express');
 
-const PORT = 8000;
-const HOST = '0.0.0.0';
+const express = require('express');
 
 const os = require('os');
 var hostname = os.hostname();
 
+const PORT = 8000;
+const HOST = '0.0.0.0';
+
 const app = express();
 app.get('/', (req, res) => {
-  res.send('HostName : ' + hostname);
+  res.send("<html><head><h1>hostname : " + hostname +
+    	"</h1></head><body><img src='https://blog.docker.com/wp-content/uploads/Swarmnado-357x627-30-1.gif'></body></html>");
 });
 
 app.listen(PORT, HOST);
